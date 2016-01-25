50d8a7322915b9a3a4036b32718424a1a0033129
Femi From Dell <Femi@paosin.com>
2016-01-24 21:49:25 -0500

    added harpoxy puppet to load balance puppet

---
+++ manifests/haproxy-puppet.pp
@@ -0,0 +1,3 @@
+class role::haproxy-puppet(){
+   include profile::haproxy-puppet  
+}
--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -2,5 +2,5 @@ class role::puppetmaster () {
   include profile::r10k_p
   # include r10k::mcollective
   include profile::puppetmaster
-
+ 
 }
